5b47fc45fb4f9ab70587329c78bcbd3edb834a41
Medo Araim <aaraim@ggc.edu>
2023-11-29 15:22:08 -0500

    Initial commit

---
+++ src/Movie.java
@@ -0,0 +1,48 @@
+public class Movie {
+    private Title title;
+    private Genre genre;
+    private Year year;
+
+    public Movie(Title title, Genre genre, Year year) {
+        this.title = title;
+        this.genre = genre;
+        this.year = year;
+    }
+
+    public Title getTitle() {
+        return title;
+    }
+
+    public void setTitle(Title title) {
+        this.title = title;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public Year getYear() {
+        return year;
+    }
+
+    public void setYear(Year year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie has a " +
+                "title = " + title +
+                ", a genre = " + genre +
+                "and, a year = " + year;
+    }
+}
+public class MovieTester {
+    public static void main(String[] args) {
+        Movie m1 = new Movie(new Title ("Nacho Libre"),new Genre("Comedy"), new Year(2006))
+    }
+}
